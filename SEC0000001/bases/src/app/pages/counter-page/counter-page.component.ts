ff9e653043438045b6d50d66be7a041c6aded121
andres.rpenuela <andresruizpenuela@gmail.com>
2025-02-28 11:15:13 +0100

    [SEC0000001-0001] example of communication basic between the view and the logic of component, with styles css

--- SEC0000001/bases/src/app/pages/counter-page/counter-page.component.ts
+++ SEC0000001/bases/src/app/pages/counter-page/counter-page.component.ts
@@ -4,8 +4,21 @@ import { Component } from '@angular/core';
   selector: 'app-counter-page',
   imports: [],
   templateUrl: './counter-page.component.html',
+  standalone: true,
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+    protected counter:number = 0;
 
+    public add(unit:number){
+      this.counter += unit;
+    }
+
+    public subtract(unit:number){
+      this.counter -= unit;
+    }
+
+    public reset(){
+      this.counter = 0;
+    }
 }
