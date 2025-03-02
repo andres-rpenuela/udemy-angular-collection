4f03fdd64b31150c06560045e3f5135a05852189
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-02 21:36:49 +0100

    [SEC0000001-0003] task 02, created dragon ball page component and configure a route

---
+++ SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+  styleUrl: './dragonball-page.component.css',
+  standalone: true
+})
+export class DragonballPageComponent {
+
+}
