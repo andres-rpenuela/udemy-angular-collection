317d3f6b6691266def9672994dcd29922ced5085
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-04 18:57:52 +0100

    [SEC0000001-0003] use for directive about array

--- SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-dragonball-page',
@@ -8,5 +8,31 @@ import { Component } from '@angular/core';
   standalone: true
 })
 export class DragonballPageComponent {
+    private _persons : person[] = [
+      {
+        id: 1,
+        name:'Goku',
+        power:300.19
+      },
+      {
+        id: 2,
+        name:'Crilin',
+        power:290.19
+      },
+      {
+        id: 3,
+        name:'Son Gohan',
+        power:290.19
+      }
+    ];
 
+    public getPerson(): person[]{
+      return this._persons;
+    }
+}
+
+interface person{
+  id: number;
+  name: string;
+  power: number;
 }
