8747a469e8583ef9f6a124a04ac543e058d374c1
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-04 20:26:25 +0100

    [SEC0000001-0003] rename interface person as Character, and updated the person arrays as signal of arrays, and insert class css to power value

--- SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, signal, Signal, WritableSignal} from '@angular/core';
 
 @Component({
   selector: 'app-dragonball-page',
@@ -8,30 +8,19 @@ import {Component} from '@angular/core';
   standalone: true
 })
 export class DragonballPageComponent {
-    private _persons : person[] = [
-      {
-        id: 1,
-        name:'Goku',
-        power:300.19
-      },
-      {
-        id: 2,
-        name:'Crilin',
-        power:290.19
-      },
-      {
-        id: 3,
-        name:'Son Gohan',
-        power:290.19
-      }
-    ];
 
-    public getPerson(): person[]{
-      return this._persons;
+    private _characters : WritableSignal<Character[]> = signal([
+      { id: 1,  name:'Goku',      power:300.19  },
+      { id: 2,  name:'Crilin',    power:290.19  },
+      { id: 3,  name:'Son Gohan', power:290.19  }
+    ]);
+
+    public getCharacters(): Character[]{
+      return this._characters();
     }
 }
 
-interface person{
+interface Character{
   id: number;
   name: string;
   power: number;
