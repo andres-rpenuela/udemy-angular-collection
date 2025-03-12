ebd5d4609ce9b139a0487c357d8df42d99c4fe33
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-13 00:16:08 +0100

    [SEC0000001-0003] task 02, inputs html with signals and events

--- SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
+++ SEC0000001/bases/src/app/pages/dragonball-page/dragonball-page.component.ts
@@ -1,5 +1,6 @@
-import {Component, computed, signal, Signal, WritableSignal} from '@angular/core';
+import {Component, computed, signal, WritableSignal} from '@angular/core';
 import {NgClass, NgIf} from '@angular/common';
+
 
 @Component({
   selector: 'app-dragonball-page',
@@ -13,25 +14,37 @@ import {NgClass, NgIf} from '@angular/common';
 })
 export class DragonballPageComponent {
 
-    private _characters : WritableSignal<Character[]> = signal([
-      { id: 1,  name:'Goku',      power:300.19  },
-      { id: 2,  name:'Crilin',    power:290  },
-      { id: 3,  name:'Son Gohan', power:290.19  },
-      { id: 3,  name:'Yancha', power:100  }
-    ]);
+  name: WritableSignal<string> = signal('name');
+  power: WritableSignal<number> = signal(100);
 
-    public getCharacters(): Character[]{
+  private _characters : WritableSignal<Character[]> = signal([
+    { id: 1,  name:'Goku',      power:300.19  },
+    { id: 2,  name:'Crilin',    power:290  },
+    { id: 3,  name:'Son Gohan', power:290.19  },
+    { id: 3,  name:'Yancha', power:100  }
+  ]);
 
-      return this._characters();
+  public getCharacters(): Character[]{
 
-    }
+    return this._characters();
+
+  }
 
   public powerClasses = computed(() => {
-      return {
-        'text-danger': true,
-        'border':true
-      }
+    return {
+      'text-danger': true,
+      'border':true
+    }
+  });
+
+  created():void{
+    this._characters.update(value =>{
+      value.push({id:this._characters.length,name:this.name(),power:this.power()})
+      return value;
     })
+  }
+
+
 }
 
 interface Character{
