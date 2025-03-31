513008f07b3cb395a26be35052271e8888b4954a
andres.rpenuela <andresruizpenuela@gmail.com>
2025-03-31 11:05:54 +0200

    [SEC0000001-0005] ejemplo de la directiva input e input con señales

--- SEC0000001/bases/src/app/components/dragonball/character-list/character-list.component.ts
+++ SEC0000001/bases/src/app/components/dragonball/character-list/character-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, Input, input, InputSignal} from '@angular/core';
 import {Character} from '../../../interfaces/character.interface';
 
 @Component({
@@ -10,8 +10,10 @@ import {Character} from '../../../interfaces/character.interface';
 })
 export class CharacterListComponent {
 
+  @Input("listTitle") public listTitle:string = '';
+  //public listTitle:InputSignal<string>  = input.required<string>();
+
   //_characters:Character[] = [{id:1,name:'Goku',power:300.0},{id:1,name:'Son Goku',power:12.4}]
+  // @Input("characters") public characters:Character[] = [];
   public characters:InputSignal<Character[]>   = input.required<Character[]>();
-
-
 }
