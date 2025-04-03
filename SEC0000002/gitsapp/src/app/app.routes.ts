720393153339ea268d480e2ac0cf86e2340fea77
andres.rpenuela <andresruizpenuela@gmail.com>
2025-04-03 10:21:58 +0200

    [SEC0000002-0003] rutas hijas con carga perezosa

--- SEC0000002/gitsapp/src/app/app.routes.ts
+++ SEC0000002/gitsapp/src/app/app.routes.ts
@@ -5,19 +5,25 @@ export const routes: Routes = [
   {
     path: 'dashboard',
     loadComponent: () =>
-      import('./gifs/pages/dashboard-page/dashboard-page.component').then(c => c.DashboardPageComponent)
-  },
-  // load lazy, opcion con "export default ..." de un componente standalone o modulo
-  {
-    path: 'trending',
-    loadComponent: () =>
-      import('./gifs/pages/trending-page/trending-page.component')
-  },
-  // load lazy, opcion con "export default ..." de un componente standalone o modulo
-  {
-    path: 'search',
-    loadComponent: () =>
-      import('./gifs/pages/search-page/search-page.component')
+      import('./gifs/pages/dashboard-page/dashboard-page.component').then(c => c.DashboardPageComponent),
+    children:[
+      // load lazy, opcion con "export default ..." de un componente standalone o modulo
+      {
+        path: 'trending',
+        loadComponent: () =>
+          import('./gifs/pages/trending-page/trending-page.component')
+      },
+      // load lazy, opcion con "export default ..." de un componente standalone o modulo
+      {
+        path: 'search',
+        loadComponent: () =>
+          import('./gifs/pages/search-page/search-page.component')
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
   // rutas por defecto
   {
