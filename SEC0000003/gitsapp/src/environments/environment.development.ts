6e1e50903dcbae589295b20664c688b3bc703a0c
andres.rpenuela <andresruizpenuela@gmail.com>
2025-04-07 11:26:49 +0200

    [SEC0000003-0001] integracion de Giphy Api Key Develop en las variables de entorno

--- SEC0000003/gitsapp/src/environments/environment.development.ts
+++ SEC0000003/gitsapp/src/environments/environment.development.ts
@@ -2,9 +2,10 @@ export const environment = {
   production: true,
   companyName: 'Gifs',
   companyName2: 'App',
-  companySlogan: 'Maneja tus gifs'
+  companySlogan: 'Maneja tus gifs',
 
   // Api Keys
+  giphyApiKey: 'NwsnniRev0MMAQFEE6KPDykViH869Q2C'
 
   // URL
 };
