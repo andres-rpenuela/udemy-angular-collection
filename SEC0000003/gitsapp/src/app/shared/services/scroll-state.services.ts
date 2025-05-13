01e2f103d8ba2d7992e30aa5f9df4389e6cc3734
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-13 23:02:51 +0200

    [SEC0000004-0003] plus para guardar el scroll en un record como alternativa al us ode propiedades

--- SEC0000003/gitsapp/src/app/shared/services/scroll-state.services.ts
+++ SEC0000003/gitsapp/src/app/shared/services/scroll-state.services.ts
@@ -15,4 +15,12 @@ export class ScrollStateService {
   getTrendingScrollState():number{
     return this.trendingScrollState();
   }
+
+  // alternativa al usar una property por página, es crear un record, donde se almacena un registro por página
+  // esto es más flexibe
+  pagesScrollStates : Record<string, number> = {
+    'page1': 0,
+    'pate2': 0,
+    'aboutPage':20 //,...
+  }
 }
