82471dabe6c290042ae65803c04894010c3de164
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-17 16:31:13 +0200

    [SEC0000005] creacion de layout, cargar todas las vistas en un componente relacioando con country, como si fuera una page, pero de country

--- SEC0000005/country-app/src/app/countries/country.routes.ts
+++ SEC0000005/country-app/src/app/countries/country.routes.ts
@@ -1,11 +1,27 @@
 import {Routes} from '@angular/router';
+import {CountryLayoutComponent} from './layouts/country-layout/country-layout.component';
 import {ByCapitalComponent} from './components/by-capital/by-capital.component';
 
 export const countryRoutes:Routes = [
   {
-  path: '',
-    component: ByCapitalComponent
-  }];
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      { // carga no perezosa
+        path: 'by-capital',
+        component: ByCapitalComponent
+      },
+      // { // cargar perezosa
+      //   path: 'by-capital',
+      //   loadComponent: () => import('./components/by-capital/by-capital.component').then(m => m.ByCapitalComponent),
+      // },
+      { // si no es ninguna de la anterior, redirecciona
+        path: '**',
+        redirectTo: 'by-capital'
+      }
+    ]
+  }
+];
 
 // lo exportamos por defecto, para que cuando se haga una carga perezosa, este lo coga por defecto y no hace falta aplicar
 // el then, es decir, haciendo 'export default country' permite pasar de eso
---
+++ SEC0000005/country-app/src/app/countries/layouts/country-layout/country-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+@Component({
+  selector: 'app-country-layout',
+  imports: [
+    RouterOutlet
+  ],
+  templateUrl: './country-layout.component.html',
+  styleUrl: './country-layout.component.css',
+  standalone: true
+})
+export class CountryLayoutComponent {
+
+}
