eba767b2083a02d9c75f4a8d8e987fe4c6297cc3
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-18 14:28:25 +0200

    [SEC0000005] buton para buscar por pais con debounce

--- SEC0000005/country-app/src/app/countries/components/by-capital/by-capital.component.ts
+++ SEC0000005/country-app/src/app/countries/components/by-capital/by-capital.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {debounceTime, distinctUntilChanged, Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-country-by-capital',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './by-capital.component.css',
   standalone: true
 })
-export class ByCapitalComponent {
+export class ByCapitalComponent implements OnInit, OnDestroy{
+  private searchSubject: Subject<string> = new Subject<string>();
+  private searchSub : Subscription | undefined;
 
+  onDeBounce(value : string){
+    this.searchSubject.next(value);
+
+  }
+  onSearch(value:string){
+    console.log(value);
+  }
+
+  ngOnInit() {
+    this.searchSub = this.searchSubject
+      .pipe(
+        debounceTime(500), // garantiza que sólo el último valor emitido
+        distinctUntilChanged() //  evita que se emita el mismo valor dos veces seguidas
+      )
+      .subscribe(valueToSend => {
+        // Aquí va tu lógica para buscar o filtrar
+        this.onSearch(valueToSend)
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.searchSub?.unsubscribe(); // evita fugas de memoria
+  }
 }
