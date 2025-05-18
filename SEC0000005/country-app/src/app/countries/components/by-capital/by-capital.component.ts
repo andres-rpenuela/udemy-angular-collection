a53a777e72ca03b2db2dd093cb13fd21a31dd6ed
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-18 13:59:40 +0200

    [SEC0000005] se añade el menu de country, mas los enlaces con routeLink

--- SEC0000005/country-app/src/app/countries/components/by-capital/by-capital.component.ts
+++ SEC0000005/country-app/src/app/countries/components/by-capital/by-capital.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-by-capital',
+  selector: 'app-country-by-capital',
   imports: [],
   templateUrl: './by-capital.component.html',
   styleUrl: './by-capital.component.css',
