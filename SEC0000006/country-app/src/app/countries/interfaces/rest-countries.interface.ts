211516963771569cc474ddc34d265caa360f280e
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-19 18:47:14 +0200

    [SEC0000006] se modifica el mapper para que ponga el nombre en spa

--- SEC0000006/country-app/src/app/countries/interfaces/rest-countries.interface.ts
+++ SEC0000006/country-app/src/app/countries/interfaces/rest-countries.interface.ts
@@ -20,7 +20,7 @@ export interface RestCountry {
   area:         number;
   demonyms:     Demonyms;
   cca3:         string;
-  translations: { [key: string]: Translation };
+  translations: { [key: string]: Translation }; // Record
   flag:         string;
   maps:         Maps;
   population:   number;
