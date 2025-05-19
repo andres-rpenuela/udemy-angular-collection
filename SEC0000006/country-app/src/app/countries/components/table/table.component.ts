d68156d5405047167a6fd66079c2a84f1d9cdfd0
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-19 18:52:56 +0200

    [SEC0000006] uso de DecimalPipe para la población

--- SEC0000006/country-app/src/app/countries/components/table/table.component.ts
+++ SEC0000006/country-app/src/app/countries/components/table/table.component.ts
@@ -1,12 +1,13 @@
 import {Component, effect, input, InputSignal} from '@angular/core';
-import {TitleCasePipe} from '@angular/common';
+import {DecimalPipe, TitleCasePipe} from '@angular/common';
 import {RestCountry} from '../../interfaces/rest-countries.interface';
 import {Country} from '../../interfaces/country.interface';
 
 @Component({
   selector: 'app-shared-table',
   imports: [
-    TitleCasePipe
+    TitleCasePipe,
+    DecimalPipe
   ],
   templateUrl: './table.component.html',
   styleUrl: './table.component.css',
