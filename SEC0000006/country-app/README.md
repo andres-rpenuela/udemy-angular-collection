e8bfe59e5d8ed56da99bdf5c3f2520f72de14dca
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-21 22:08:52 +0200

    [SEC0000006] uso debounce con señales y efecto

--- SEC0000006/country-app/README.md
+++ SEC0000006/country-app/README.md
@@ -168,3 +168,90 @@ Uso en html
   </app-shared-table>
 </div>
 ```
+
+---
+
+# Debounce
+
+## Con Observables
+
+Se crea un "Subject" y una "Subcripcion"
+```typescript
+private debounceSubject: Subject<string> = new Subject<string>();
+private debounceSubscription: Subscription | undefined;
+
+
+ngOnInit() {
+
+  this.debounceSubject.pipe(
+    debounceTime(500),
+    distinctUntilChanged(),
+  ).subscribe( value => this.emitValue(value) )
+}
+
+ngOnDestroy() {
+  this.debounceSubscription?.unsubscribe();
+}
+
+public onDeBounce(value : string){
+  this.debounceSubject.next(value);
+}
+
+public emitValue(value : string){
+  console.log(value);
+  this.valueEmmit.emit(value);
+}
+```
+En la vista del componente, se invoca al metodo "onDeBounce"
+```angular181html
+<input
+  type="text"
+  class="input input-bordered mr-2 w-56"
+  [placeholder]="placeholderSearch()"
+  autofocus
+  #inputCapital
+  (keyup)="onDeBounce(inputCapital.value)"
+>
+```
+
+## Con Señales
+
+```typescript
+valueSignal = signal<string>('');
+debounceTime = input(300);
+
+// se lanza el efecto cada vez que se destruya el componente, crea y cada vez que el valueSignla cambie
+// Si en ese efecto has creado un setTimeout, un interval, o algún otro recurso que debe limpiarse
+// antes de volver a ejecutar el efecto (para evitar fugas de memoria o duplicados), usás onClean
+debounceEffect = effect( (onCleanup)=>{
+  // cada vez que cambia lanza el effecto
+  const value = this.valueSignal(); // accede a la señal reactiva
+
+  // espera X ms
+  const timeout = setTimeout( () =>{
+    this.valueEmmit.emit(value);
+  }, this.debounceTime());
+
+  // Este código se ejecuta justo antes de que el efecto se vuelva a lanzar
+  // entonces cada vez que this.valueSignal()
+  // 1. Se limpia el timeout anterior con clearTimeout.
+  // 2. Se programa un nuevo setTimeout con el nuevo valor.
+  // 3. Si el valor no cambia dentro del tiempo (debounceTime), se emite.
+  onCleanup( () => {
+    clearTimeout(timeout); // limpia el timeout anterior
+  })
+})
+```
+
+En la vista html:
+
+```angular181html
+<input
+  type="text"
+  class="input input-bordered mr-2 w-56"
+  [placeholder]="placeholderSearch()"
+  autofocus
+  #inputCapital
+  (keyup)="valueSignal.set(inputCapital.value)"
+>
+```
--- SEC0000006/country-app/src/app/shared/components/search/search.component.ts
+++ SEC0000006/country-app/src/app/shared/components/search/search.component.ts
@@ -1,5 +1,15 @@
-import {Component, input, InputSignal, OnDestroy, OnInit, output, OutputEmitterRef} from '@angular/core';
-import {debounceTime, distinctUntilChanged, Subject, Subscription} from 'rxjs';
+import {
+  Component,
+  effect,
+  input,
+  InputSignal,
+  OnDestroy,
+  OnInit,
+  output,
+  OutputEmitterRef,
+  signal
+} from '@angular/core';
+import {debounceTime, distinctUntilChanged, Subject, Subscription, timeout} from 'rxjs';
 
 @Component({
   selector: 'app-shared-search',
@@ -13,29 +23,59 @@ export class SearchComponent implements OnInit, OnDestroy{
 
   public placeholderSearch : InputSignal<string> = input.required<string>();
 
-  private debounceSubject: Subject<string> = new Subject<string>();
-  private debounceSubscription: Subscription | undefined;
-
-
-  ngOnInit() {
-
-    this.debounceSubject.pipe(
-      debounceTime(500),
-      distinctUntilChanged(),
-    ).subscribe( value => this.emitValue(value) )
-  }
-
-  ngOnDestroy() {
-    this.debounceSubscription?.unsubscribe();
-  }
-
-  public onDeBounce(value : string){
-    this.debounceSubject.next(value);
-  }
-
+  // private debounceSubject: Subject<string> = new Subject<string>();
+  // private debounceSubscription: Subscription | undefined;
+  //
+  //
+  // ngOnInit() {
+  //
+  //   this.debounceSubject.pipe(
+  //     debounceTime(500),
+  //     distinctUntilChanged(),
+  //   ).subscribe( value => this.emitValue(value) )
+  // }
+  //
+  // ngOnDestroy() {
+  //   this.debounceSubscription?.unsubscribe();
+  // }
+  //
+  // public onDeBounce(value : string){
+  //   this.debounceSubject.next(value);
+  // }
+  //
   public emitValue(value : string){
     console.log(value);
     this.valueEmmit.emit(value);
   }
 
+  valueSignal = signal<string>('');
+  debounceTime = input(300);
+
+  // se lanza el efecto cada vez que se destruya el componente, crea y cada vez que el valueSignla cambie
+  // Si en ese efecto has creado un setTimeout, un interval, o algún otro recurso que debe limpiarse
+  // antes de volver a ejecutar el efecto (para evitar fugas de memoria o duplicados), usás onClean
+  debounceEffect = effect( (onCleanup)=>{
+    // cada vez que cambia lanza el effecto
+    const value = this.valueSignal(); // accede a la señal reactiva
+
+    // espera X ms
+    const timeout = setTimeout( () =>{
+      this.valueEmmit.emit(value);
+    }, this.debounceTime());
+
+    // Este código se ejecuta justo antes de que el efecto se vuelva a lanzar
+    // entonces cada vez que this.valueSignal()
+    // 1. Se limpia el timeout anterior con clearTimeout.
+    // 2. Se programa un nuevo setTimeout con el nuevo valor.
+    // 3. Si el valor no cambia dentro del tiempo (debounceTime), se emite.
+    onCleanup( () => {
+      clearTimeout(timeout); // limpia el timeout anterior
+    })
+  })
+
+  ngOnDestroy(): void {
+  }
+
+  ngOnInit(): void {
+  }
 }
