732dbb8df56c1cedd1551c6fe995505701bb3d60
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-25 18:31:26 +0200

    [SEC0000007] uppercase, lowercase y tittle case pipes ejemplos

--- SEC0000007/pipes-demo-app/src/app/pages/basic-page/basic-page.component.ts
+++ SEC0000007/pipes-demo-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-basic-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    UpperCasePipe,
+    TitleCasePipe
+  ],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css',
   standalone: true
 })
 export default class BasicPageComponent {
-
+    nameLower = signal('andres');
+    nameUpper = signal('ANDRES');
+    fullName= signal('anDreS RuiZ PeÑueLa')
 }
