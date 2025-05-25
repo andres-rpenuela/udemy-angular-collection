57dd9726744504df8384e93974525d3a593c14e8
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-25 19:21:14 +0200

    [SEC0000007] registrar idiomas + camibar el idoma por defecto

--- SEC0000007/pipes-demo-app/README.md
+++ SEC0000007/pipes-demo-app/README.md
@@ -133,3 +133,32 @@ npm install daisyui@latest --force
 @import "tailwindcss";
 @plugin "daisyui";
 ```
+
+---
+
+# Cambiar de iomas
+
+En la clase de configuracion del proyecto, se debe cargar y registrar los idimas deseados, estos idiomas, pueden ser usdaos del ´@angular/common`
+
+```typescript
+//... 
+// registrar idiomas
+import {registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs, 'es' );
+registerLocaleData(localeFr, 'fr' );
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // selecionar idoma por defecto, por fecto es el ingles
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ]
+};
+```
--- SEC0000007/pipes-demo-app/src/app/app.config.ts
+++ SEC0000007/pipes-demo-app/src/app/app.config.ts
@@ -1,8 +1,24 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {ApplicationConfig, LOCALE_ID, provideZoneChangeDetection} from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
+// registrar idiomas
+import {registerLocaleData} from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+import localeFr from '@angular/common/locales/fr';
+
+registerLocaleData(localeEs, 'es' );
+registerLocaleData(localeFr, 'fr' );
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    // selecionar idoma por defecto, por fecto es el ingles
+    {
+      provide: LOCALE_ID,
+      useValue: 'es'
+    }
+  ]
 };
