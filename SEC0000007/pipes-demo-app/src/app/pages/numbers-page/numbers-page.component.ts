5cfc8a935dd1903d34ab6ba093e8158c843ea134
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-25 18:46:20 +0200

    [SEC0000007] decimal (number), percent y currency pipes ejemplo

--- SEC0000007/pipes-demo-app/src/app/pages/numbers-page/numbers-page.component.ts
+++ SEC0000007/pipes-demo-app/src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
+import {CurrencyPipe, DecimalPipe, LowerCasePipe, PercentPipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [
+    LowerCasePipe,
+    TitleCasePipe,
+    UpperCasePipe,
+    DecimalPipe,
+    PercentPipe,
+    CurrencyPipe
+  ],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css',
   standalone: true
 })
 export default class NumbersPageComponent {
 
+  totalSells = signal(2_433_232.5567);
+  percent = signal(0.4856);
 }
