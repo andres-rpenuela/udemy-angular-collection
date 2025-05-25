3fd91d1df533a22743f7739d8087421e1b147710
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-25 18:59:55 +0200

    [SEC0000007] date pipe + effect for updated signal + clear interval

--- SEC0000007/pipes-demo-app/src/app/pages/basic-page/basic-page.component.ts
+++ SEC0000007/pipes-demo-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,19 +1,36 @@
-import {Component, signal} from '@angular/core';
-import {LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
+import {Component, effect, signal} from '@angular/core';
+import {DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe} from '@angular/common';
+import {interval} from 'rxjs';
 
 @Component({
   selector: 'app-basic-page',
   imports: [
     LowerCasePipe,
     UpperCasePipe,
-    TitleCasePipe
+    TitleCasePipe,
+    DatePipe
   ],
   templateUrl: './basic-page.component.html',
   styleUrl: './basic-page.component.css',
   standalone: true
 })
 export default class BasicPageComponent {
-    nameLower = signal('andres');
-    nameUpper = signal('ANDRES');
-    fullName= signal('anDreS RuiZ PeÑueLa')
+  nameLower = signal('andres');
+  nameUpper = signal('ANDRES');
+  fullName= signal('anDreS RuiZ PeÑueLa')
+
+  // fechas
+  currentDate = signal(new Date());
+  tickingDateEffect = effect( (onCleanup) =>{
+    const interval = setInterval( () =>{
+      this.currentDate.set(new Date());
+      console.log('tick')
+    },1000);
+
+    // cuando se destruya el efecto,
+    // elimina el intervalo
+    onCleanup( () => {
+      interval
+    });
+  })
 }
