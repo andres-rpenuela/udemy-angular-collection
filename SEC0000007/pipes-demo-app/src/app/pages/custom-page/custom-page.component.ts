3f3f411ed58fe29c49dced5e690ed799c25c9f20
andres.rpenuela <andresruizpenuela@gmail.com>
2025-05-28 22:33:50 +0200

    [SEC0000008] se carga data + interfaz + se muestra la vista de heros en custom pipes

--- SEC0000007/pipes-demo-app/src/app/pages/custom-page/custom-page.component.ts
+++ SEC0000007/pipes-demo-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, signal, WritableSignal} from '@angular/core';
 import {ToggleCasePipe} from '../../pipes/toggle-case.pipe.ts';
+import {heroes} from '../../data/hero.data';
+import {Hero} from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-custom-page',
@@ -15,7 +17,13 @@ export default class CustomPageComponent {
   name = signal('Andres Ruiz');
   upperCase = signal(true);
 
+  heros:WritableSignal<Hero[]> = signal<Hero[]>(heroes);
+
   public toggle(){
     this.upperCase.update(value => !value);
+  }
+
+  public getHeros(): Hero[]{
+    return this.heros();
   }
 }
