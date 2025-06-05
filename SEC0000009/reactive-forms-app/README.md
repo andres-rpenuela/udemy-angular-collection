bb0a445d57c7b0ff247acb64ef1399c19c896401
andres.rpenuela <andresruizpenuela@gmail.com>
2025-06-05 22:05:32 +0200

    [SEC0000009] subcripción a cambios del control de un formulario

--- SEC0000009/reactive-forms-app/README.md
+++ SEC0000009/reactive-forms-app/README.md
@@ -1217,3 +1217,59 @@ public myForm :FormGroup = this.formBuilder.group({
   }
 );
 ```
+
+-- 
+## Subcripción a cambios del formulario
+
+### Opción A.
+
+```typescript
+// Es un Subject<void> que actúa como notificador de destrucción para todas las suscripciones.
+private destroy$ = new Subject<void>();
+
+public myForm : FormGroup = this.formBuilder.group({
+  region:['', Validators.required],
+  country:['', Validators.required],
+  border:['', Validators.required]
+})
+
+// opcion A: Con subcripción (Requiere destrucción
+public formRegionChanged = this.myForm.get('region')!.valueChanges
+  .pipe(takeUntil(this.destroy$)) // se cancela o desuscribe automáticamente esa suscripción.
+  .subscribe(value =>{
+    console.log('Opciona A: '+value);
+  });
+
+ngOnDestroy() {
+  //  patrón común en Angular para cancelar suscripciones automáticamente cuando el componente se destruye
+  this.destroy$.next();     // dispara cancelaciones
+  this.destroy$.complete(); // cierra el subject
+}
+```
+
+### Opción B.
+
+```typescript
+public myForm : FormGroup = this.formBuilder.group({
+  region:['', Validators.required],
+  country:['', Validators.required],
+  border:['', Validators.required]
+})
+
+// Opcion B: Con señales + efectos (no hace falta usar OnDestroy
+onFormChanged = effect( ( onCleanup ) =>{
+  // similiar a la opción A
+  const formRegionChanged = this.myForm.get('region')!.valueChanges
+    .subscribe(value =>{
+      console.log('Opcion B:'+value);
+    });
+
+  // Se llama cuando el efecto es destruido
+  onCleanup( ()=>{
+    formRegionChanged.unsubscribe();
+    console.log('Limpiando')
+  });
+})
+
+
+```
--- SEC0000009/reactive-forms-app/src/app/country/page/country-page/country-page.component.ts
+++ SEC0000009/reactive-forms-app/src/app/country/page/country-page/country-page.component.ts
@@ -1,8 +1,9 @@
-import {Component, inject, signal} from '@angular/core';
+import {Component, effect, inject, OnDestroy, signal} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {JsonPipe} from '@angular/common';
 import {CountryService} from '../../services/country.service';
 import {Country} from '../../interfaces/country';
+import {Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -11,7 +12,7 @@ import {Country} from '../../interfaces/country';
   styleUrl: './country-page.component.css',
   standalone: true
 })
-export class CountryPageComponent {
+export class CountryPageComponent implements OnDestroy{
 
   private formBuilder = inject(FormBuilder);
   private countryService = inject(CountryService);
@@ -20,9 +21,40 @@ export class CountryPageComponent {
   countriesByRegion = signal<Country[]>([]);
   borders = signal<Country[]>([]);
 
+  // Es un Subject<void> que actúa como notificador de destrucción para todas las suscripciones.
+  private destroy$ = new Subject<void>();
+
   public myForm : FormGroup = this.formBuilder.group({
     region:['', Validators.required],
     country:['', Validators.required],
     border:['', Validators.required]
   })
+
+  // opcion A: Con subcripción (Requiere destrucción
+  public formRegionChanged = this.myForm.get('region')!.valueChanges
+    .pipe(takeUntil(this.destroy$)) // se cancela o desuscribe automáticamente esa suscripción.
+    .subscribe(value =>{
+      console.log('Opciona A: '+value);
+    });
+
+  // Opcion B: Con señales + efectos (no hace falta usar OnDestroy
+  onFormChanged = effect( ( onCleanup ) =>{
+    // similiar a la opción A
+    const formRegionChanged = this.myForm.get('region')!.valueChanges
+      .subscribe(value =>{
+        console.log('Opcion B:'+value);
+      });
+
+    // Se llama cuando el efecto es destruido
+    onCleanup( ()=>{
+        formRegionChanged.unsubscribe();
+        console.log('Limpiando')
+    });
+  })
+
+  ngOnDestroy() {
+    //  patrón común en Angular para cancelar suscripciones automáticamente cuando el componente se destruye
+    this.destroy$.next();     // dispara cancelaciones
+    this.destroy$.complete(); // cierra el subject
+  }
 }
