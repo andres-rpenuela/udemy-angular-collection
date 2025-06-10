fd869e4246fbcb5dda527d8394c82965e0782563
andres.rpenuela <andresruizpenuela@gmail.com>
2025-06-10 18:15:24 +0200

    [SEC0000011] control de zoom: uso de señales + listener

--- SEC0000011/maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
+++ SEC0000011/maps-app/src/app/pages/fullscreen-map-page/fullscreen-map-page.component.ts
@@ -1,16 +1,31 @@
-import {AfterViewInit, Component, ElementRef, viewChild} from '@angular/core';
+import {AfterViewInit, Component, effect, ElementRef, linkedSignal, signal, viewChild} from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 import {environment} from '../../../environments/environment';
+import {DecimalPipe} from '@angular/common';
 
 @Component({
   selector: 'app-fullscreen-map-page',
-  imports: [],
+  imports: [
+    DecimalPipe
+  ],
   templateUrl: './fullscreen-map-page.component.html',
   styleUrl: './fullscreen-map-page.component.css'
 })
 export class FullscreenMapPageComponent implements AfterViewInit {
 
   private divFullscreenMap   = viewChild<ElementRef>('map');
+
+  protected map = signal<mapboxgl.Map|null>(null);
+  // Signal to hold the map instance, initially null
+  protected zoom = linkedSignal( () => this.map()?.getZoom() || 14 );
+
+  private zoomEffect = effect(() => {
+    if( !this.map() || !this.map()?.getZoom() ) return;
+
+
+    this.map()!.setZoom(this.zoom()); // clasic
+    //this.map()!.zoomTo(this.zoom()); // effect
+  });
 
   // cuando se inicializa el componente y la vista está lista, se ejecuta este metodo
   // lo hazmoes asincrono para que se pueda esperar a que el DOM esté completamente cargado
@@ -41,9 +56,25 @@ export class FullscreenMapPageComponent implements AfterViewInit {
     const map = new mapboxgl.Map({
       container: element, // Use the viewChild to get the map container
       style: 'mapbox://styles/mapbox/streets-v12',
-      zoom: 9,
+      zoom: this.zoom(),
       center: [-74.5, 40]
     });
 
+    //this.map.set(map)
+    this.listenZoomIn(map);
+  }
+
+  // Method to handle zoom in
+  private listenZoomIn(map: mapboxgl.Map) {
+    if( !map ) return;
+
+    // Listen for zoom events and update the zoom signal
+    map.on('zoom', () => {
+      const newZoom = map.getZoom();
+      this.zoom.set(newZoom); // Update the zoom signal with the new zoom level
+    });
+
+    // Set the map instance to the signal
+    this.map.set(map)
   }
 }
