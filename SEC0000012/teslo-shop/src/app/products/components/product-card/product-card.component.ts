ee3d141d3b7a1ee3a863de07f6d42112886e30ba
andres.rpenuela <andresruizpenuela@gmail.com>
2025-06-19 18:22:18 +0200

    [SEC0000012] resolución de tarea por el profesro + carga de images basico

--- SEC0000012/teslo-shop/src/app/products/components/product-card/product-card.component.ts
+++ SEC0000012/teslo-shop/src/app/products/components/product-card/product-card.component.ts
@@ -1,6 +1,7 @@
-import {Component, input, InputSignal} from '@angular/core';
+import {Component, computed, input, InputSignal} from '@angular/core';
 import {RouterLink} from '@angular/router';
-import {TruncatePipe} from '../../../pipes/truncate-pipe';
+import {TruncatePipe} from '@pipe/truncate-pipe';
+import {Product} from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'product-card',
@@ -12,8 +13,9 @@ import {TruncatePipe} from '../../../pipes/truncate-pipe';
   styleUrl: './product-card.component.css'
 })
 export class ProductCardComponent {
-  title : InputSignal<string> = input.required<string>();
-  description : InputSignal<string> = input.required<string>();
-  images : InputSignal<string[]> = input<string[]>([]);
-  id: InputSignal<string> = input.required<string>();
+  product : InputSignal<Product> = input.required<Product>();
+
+  imageUrl = computed( () =>{
+    return `http://localhost:3000/api/files/product/${ this.product().images[0] }`
+  })
 }
