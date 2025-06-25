d8667ed1c90d8585849e192ba07457dff16f09f6
andres.rpenuela <andresruizpenuela@gmail.com>
2025-06-25 17:36:36 +0200

    [SEC0000012] paginacion con rxResource

--- SEC0000012/teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ SEC0000012/teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -4,11 +4,12 @@ import {ProductCardComponent} from '@products/components/product-card/product-ca
 import {ProductsService} from '@products/services/products.service';
 // import {ProductCardComponent} from '../../../products/components/product-card/product-card.component';
 
-import { rxResource } from '@angular/core/rxjs-interop';
+import {rxResource, toSignal} from '@angular/core/rxjs-interop';
 import {ProductResponse} from '@products/interfaces/product.interface';
 import {JsonPipe} from '@angular/common';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 import {PaginationComponent} from "@shared/components/pagination/pagination.component";
+import {map} from "rxjs";
 
 @Component({
   selector: 'app-home-page',
@@ -24,12 +25,34 @@ export class HomePageComponent {
 
   private productsService = inject(ProductsService);
   private router = inject(Router);
+
+  private activateRoute = inject(ActivatedRoute);
+  // convierte un observable a una señal
+  public currentPage = toSignal( this.activateRoute.queryParamMap
+    .pipe(
+      // comvierte a numero o devuelve 1 si es undefined
+      map( params => params.get('page') ? +params.get('page')!:  1 ),
+      // comprueba que page sea un numero (devuelv nan, si no es un numero)
+      map( page => isNaN(page) ? 1 : page)
+    ),
+    {
+      initialValue: 1
+    }
+  )
+
+
   // En Angular 20
   // productsResource = rxResource({
   //   stream: () => { return this.productsService.getProducts() }
   // });
+  // Sin parámetros
+  // public productResource: ResourceRef<ProductResponse | undefined> = rxResource({
+  //   stream: ({}) => this.productsService.getProducts( {} )
+  // });
+  // Paginacion
   public productResource: ResourceRef<ProductResponse | undefined> = rxResource({
-    stream: ({}) => this.productsService.getProducts( {} )
+    params: () => this.currentPage() -1,
+    stream: ({params: currentPage}) => this.productsService.getProducts( {offset: currentPage*9} )
   });
 
   // redirect to page prodcut
