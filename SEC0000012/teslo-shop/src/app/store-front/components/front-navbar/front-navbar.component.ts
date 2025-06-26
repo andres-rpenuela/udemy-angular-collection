3af584c6c5a3aa5fc9dbc4a03694e576a0d20d7e
andres.rpenuela <andresruizpenuela@gmail.com>
2025-06-26 21:40:31 +0200

    [SEC0000014] fix product with null

--- SEC0000012/teslo-shop/src/app/store-front/components/front-navbar/front-navbar.component.ts
+++ SEC0000012/teslo-shop/src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import {FrontNavbarItemsComponent} from './front-navbar-items/front-navbar-items.component';
 import {StoreRoute} from '../../interfaces/store-route.interface';
 import {storeRoutesData} from '../../data/store-route.data';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'front-navbar',
   imports: [
     FrontNavbarItemsComponent,
-    RouterLink,
-    RouterLinkActive
+    RouterLink
   ],
   templateUrl: './front-navbar.component.html',
   styleUrl: './front-navbar.component.css'
