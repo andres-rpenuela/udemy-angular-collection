34aa5ddaf82545956e50a4919607f2a0eaaf6cc1
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-03 21:53:52 +0200

    [SEC0000015] listado de productos en una tabla + paginacion + visualizar imanges + ir al producto como administrador

--- SEC0000012/teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ SEC0000012/teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject, linkedSignal} from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import {ProductTableComponent} from '@products/components/product-table/product-table.component';
+import { ProductsService } from '@products/services/products.service';
+import {PaginationComponent} from '@shared/components/pagination/pagination.component';
+import {PaginationService} from '@shared/components/pagination/pagination.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [],
+  imports: [
+    ProductTableComponent,
+    PaginationComponent,
+    RouterLink
+  ],
   templateUrl: './products-admin-page.component.html',
   styleUrl: './products-admin-page.component.css'
 })
 export class ProductsAdminPageComponent {
+  private productService = inject(ProductsService);
+  protected paginationService = inject(PaginationService);
+
+
+  productsResource = rxResource({
+    params: () => this.paginationService.currentPage() -1,
+    stream: ({params: currentPage}) => this.productService.getProducts( {offset: currentPage*9} )
+  });
 
 }
