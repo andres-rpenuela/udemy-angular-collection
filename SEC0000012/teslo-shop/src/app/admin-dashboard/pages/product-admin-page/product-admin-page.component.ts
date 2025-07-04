20bd611f8f103260c651c34fc7583c7e1900470d
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-04 16:59:23 +0200

    [SEC0000015] lecuta del id que esta en el patch "/admin/product/:id" + redirecicón si no carga el producto

--- SEC0000012/teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ SEC0000012/teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, ResourceRef} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {rxResource, toSignal} from '@angular/core/rxjs-interop';
+import {map} from 'rxjs';
+import {ProductsService} from '@products/services/products.service';
+import {Product} from '@products/interfaces/product.interface';
 
 @Component({
   selector: 'app-product-admin-page',
@@ -7,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrl: './product-admin-page.component.css'
 })
 export class ProductAdminPageComponent {
+
+  private activatedRouter = inject(ActivatedRoute);
+  private router = inject(Router);
+
+  private productsService = inject(ProductsService);
+
+  // toSignal convierte un Observable en una señal de Angular, y le el :id del path
+  idProduct = toSignal( this.activatedRouter.paramMap.pipe(
+    map( params => params.get('id') )
+  ), {initialValue: null} )
+
+  redirectEffect = effect( () => {
+    if(!this.idProduct() || this.productResource.error() ) {
+      this.router.navigateByUrl('/admin/products');
+    }
+    console.log('ID del producto:', this.idProduct());
+  });
+
+  productResource: ResourceRef<Product | undefined> = rxResource({
+    params: () => ({
+      idSlug: this.idProduct()
+    }),
+    stream: ( { params: { idSlug } } ) =>
+      this.productsService.getProductByIdSlug( idSlug )
+  });
 
 }
