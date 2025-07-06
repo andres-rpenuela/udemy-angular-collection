e15cdfd333b0b65287bcd48647cab6313d285806
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-06 17:45:04 +0200

    [SEC0000016]  change detection - onPush

--- SEC0000013/my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ SEC0000013/my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {TitleComponent} from '@shared/title/title.component';
+import {JsonPipe} from '@angular/common';
 
 @Component({
   selector: 'app-change-detection',
-  imports: [],
+  imports: [
+    TitleComponent,
+    JsonPipe
+  ],
   templateUrl: './change-detection.component.html',
-  styles: ``
+  styles: ``,
+  //   changeDetection: ChangeDetectionStrategy.Default // Forma tradicional, se activa en todos los ciclos
+  changeDetection: ChangeDetectionStrategy.OnPush  // Zonless, solo observa los cambios de algunos ciclos
 })
 export default class ChangeDetectionComponent {
+  // Forma tradicional
+  public frameworkProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  };
+  // Angular 15+
+  public frameworkSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  constructor() {
+    setTimeout(()=>{
+      console.log('Hecho');
+      // Se detectara el cambio si el changeDetection: ChangeDetectionStrategy.OnPush
+      this.frameworkProperty =({
+          ...this.frameworkProperty,
+          name: 'React'
+      });
+      // Se detectara el cambio tanto en ChangeDetectionStrategy.OnPush como en ChangeDetectionStrategy.Default
+      this.frameworkSignal.update( (value: {name:string,releaseDate:number}) =>({
+          ...value,
+          name: 'React'
+        })
+      )
+    },2000)
+  }
+
 
 }
