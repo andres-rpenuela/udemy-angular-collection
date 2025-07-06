d52fbceb3fc737325643e6e7c88dc8ba3e0542f4
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-06 12:25:43 +0200

    [SEC0000016] se monta la estructura de la aplicacion

---
+++ SEC0000013/my-dashboard/src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-detection',
+  imports: [],
+  templateUrl: './change-detection.component.html',
+  styles: ``
+})
+export class ChangeDetectionComponent {
+
+}
---
+++ SEC0000013/my-dashboard/src/app/dashboard/pages/view-transiction/view-transiction.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-transiction',
+  imports: [],
+  templateUrl: './view-transiction.component.html',
+  styles: ``
+})
+export class ViewTransictionComponent {
+
+}
