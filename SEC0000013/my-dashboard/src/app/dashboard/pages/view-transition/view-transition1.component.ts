52bcceb231bafbed4cbbeb031f050e249e8e8717
andres.rpenuela <andresruizpenuela@gmail.com>
2025-07-07 17:19:37 +0200

    [SEC0000016]  HERO ANIMATION - view transitions - part 2

--- SEC0000013/my-dashboard/src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ SEC0000013/my-dashboard/src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -11,9 +11,14 @@ import {TitleComponent} from '@shared/title/title.component';
 
 
     <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"/>
+      <img srcset="https://picsum.photos/id/237/200/300"
+           alt="Picsum"
+           width="200"
+           height="300"
+           style="view-transition-name:hero1"/>
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div class="fixed bottom-10 left-70 bg-blue-800 w-32 h-32 rounded"
+           style="view-transition-name:hero1"></div>
     </section>
   `,
   styles: ``
--- SEC0000013/my-dashboard/src/app/dashboard/pages/view-transition/view-transition2.component.ts
+++ SEC0000013/my-dashboard/src/app/dashboard/pages/view-transition/view-transition2.component.ts
@@ -10,10 +10,16 @@ import {TitleComponent} from '@shared/title/title.component';
     <shared-title [title]="'View Trnasition 2'"></shared-title>
 
 
-    <section class="flex justify-start">
-      <img srcset="https://picsum.photos/id/237/200/300" alt="Picsum" width="200" height="300"/>
+    <section class="flex justify-end">
+      <img srcset="https://picsum.photos/id/237/200/300"
+           alt="Picsum"
+           width="200"
+           height="300"
+           style="view-transition-name:hero2"
+      />
 
-      <div class="bg-blue-500 w-56 h-56"></div>
+      <div class="bg-blue-500 w-56 h-56"
+           style="view-transition-name:hero2"></div>
     </section>
   `,
   styles: ``
